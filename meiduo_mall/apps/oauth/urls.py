8f4f8d685c09b5ff6b9ded342d8aead5ded3e9a7
tao-bug <897384405@qq.com>
2019-11-05 11:42:27 +0800

    code -- token -- openid

--- meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/apps/oauth/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # 1.获取 qq网址
     url(r'^qq/login/$', views.QQAuthURLView.as_view(), name='qqlogin'),
+    # 2.回调网址 oauth_callback
+    url(r'^oauth_callback/$', views.QQAuthCallBackView.as_view()),
 
 ]
--- meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/apps/oauth/views.py
@@ -5,6 +5,32 @@ from django.shortcuts import render
 from django.views import View
 
 
+# 用户扫码登录的回调处理QQAuthUserView
+# code--token--openid--是否绑定
+class QQAuthCallBackView(View):
+    def get(self, request):
+        # / oauth_callback?code = 31FFB8402D24214889BA444821D3CC3E & state = % 2Finfo % 2F
+        # 1.解析code
+        code = request.GET.get('code')
+
+        if not code:
+            return http.HttpResponseForbidden('无效的code!')
+
+        # 获取QQ登录页面网址
+        oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                        client_secret=settings.QQ_CLIENT_SECRET,
+                        redirect_uri=settings.QQ_REDIRECT_URI,
+                        state=next)
+        # code --- token
+        token = oauth.get_access_token(code)
+
+        # token --- openid
+        openid = oauth.get_open_id(token)
+
+        return http.HttpResponse(openid)
+
+
+# 返回qq登录地址
 class QQAuthURLView(View):
     """提供QQ登录页面网址
     https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=xxx&redirect_uri=xxx&state=xxx
@@ -18,7 +44,7 @@ class QQAuthURLView(View):
                         client_secret=settings.QQ_CLIENT_SECRET,
                         redirect_uri=settings.QQ_REDIRECT_URI,
                         state=next)
-
+        # 获取QQ登录扫码页面，扫码后得到Authorization Code
         login_url = oauth.get_qq_url()
 
         return http.JsonResponse({'code': 0, 'errmsg': 'QQ登录页面网址', 'login_url': login_url})
