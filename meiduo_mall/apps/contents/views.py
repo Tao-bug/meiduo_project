08044a3da4e2416c29ef4d43e9cc7fb294bc678b
tao-bug <897384405@qq.com>
2019-11-07 18:43:14 +0800

    查询首页商品广告

--- meiduo_mall/apps/contents/views.py
+++ meiduo_mall/apps/contents/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+
+from apps.contents.models import ContentCategory
 from apps.contents.utils import get_categories
 
 
@@ -11,6 +13,12 @@ class IndexView(View):
         """提供首页广告界面"""
         # 查询商品频道和分类
         categories = get_categories()
+
+        # 获取广告内容
+        contents = {}
+        content_categories =ContentCategory.objects.all()
+        for cat in content_categories:
+            contents[cat.key] = cat.content_set.filter(status=True).order_by('sequence')
 
         # 渲染模板的上下文
         context = {
